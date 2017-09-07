8cd03dd2d854223d436f1f96f2a496b15b3e4276
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-09-07 10:43:46 +0900

    carとsuportsCarを作った

---
+++ OverridingTest/Car.swift
@@ -0,0 +1,13 @@
+//
+//  Car.swift
+//  OverridingTest
+//
+//  Created by Atsuo Yonehara on 2017/09/07.
+//  Copyright © 2017年 Atsuo Yonehara. All rights reserved.
+//
+
+import UIKit
+
+class Car: NSObject {
+
+}
---
+++ OverridingTest/SportsCar.swift
@@ -0,0 +1,13 @@
+//
+//  SportsCar.swift
+//  OverridingTest
+//
+//  Created by Atsuo Yonehara on 2017/09/07.
+//  Copyright © 2017年 Atsuo Yonehara. All rights reserved.
+//
+
+import UIKit
+
+class SportsCar: Car {
+
+}
