5dedf345dc2ba301b006e321f3fe8be6f4b553d0
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-09-07 11:29:42 +0900

    carに詳細追加

--- OverridingTest/Car.swift
+++ OverridingTest/Car.swift
@@ -11,21 +11,28 @@ import UIKit
 class Car: NSObject {
     var name: String!
     var wheel: Int!
-    var gasoline: Int!
+    var gasoline: Double!
+    var fuelConsumption: Double!
+    var distance: Double {
+        return gasoline * fuelConsumption
+    }
     
-    init(name:String, wheel:Int, gasoline: Int) {
+    init(name:String, gasoline: Double, fuelConsumption: Double) {
         self.name = name
-        self.wheel = wheel
+        self.wheel = 4
         self.gasoline = gasoline
+        self.fuelConsumption = fuelConsumption
     }
     
     convenience override init() {
-        self.init(name: "一般車", wheel: 4, gasoline: 20)
+        self.init(name: "一般車", gasoline: 10, fuelConsumption: 10)
     }
     
     func infomation(){
         print("名前 : " + name)
         print("タイヤの数 : " + wheel.description + "つ")
         print("ガソリンの量 : " + gasoline.description + "リットル")
+        print("燃費 : 1リットル当たり" + fuelConsumption.description + "キロメートル")
+        print("走行可能距離 : " + distance.description + "キロメートル")
     }
 }
